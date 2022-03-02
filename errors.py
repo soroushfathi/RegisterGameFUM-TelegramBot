8f54c38dc8bfeb5040d1537ddf29cc57d6df39e8
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-03-02 22:47:54 +0330

    errors handling

---
+++ errors.py
@@ -0,0 +1,14 @@
+class LeaderLoginError(Exception):
+    pass
+
+
+class LoginedUserSigninError(Exception):
+    pass
+
+
+class MemberCountLimitError(Exception):
+    pass
+
+
+class ActivateUserLoginError(Exception):
+    pass
